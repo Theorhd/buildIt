64f0b5849a1d9186d1d07eedb3479112f5327f90
Theo <theo.richard8654@gmail.com>
2024-12-12 12:47:39 +0100

    Ajout couleurs personnalisées dans la configuration TailwindCSS

--- BuildIT-Frontend/tailwind.config.js
+++ BuildIT-Frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f4f4f4",
+        secondary: "#34d399",
+        bgPrimary: "#313131",
+        bgSecondary: "#1e1e1e",
+      },
+    },
   },
   plugins: [],
 };
