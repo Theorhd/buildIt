eb9752a0878a83fb45b28069c4185126c3467fd6
Julien Charp <j.charpentier@outlook.fr>
2024-12-12 16:58:25 +0100

    Add comp List

--- BuildIT-Frontend/src/components/List.tsx
+++ BuildIT-Frontend/src/components/List.tsx
@@ -4,6 +4,7 @@ import "../styles/List.css";
 export default function List() {
   const [list, setList] = useState<string[]>([]);
   const [task, setTask] = useState<string>("");
+  const [Tags, setTags] = useState<string>("Tags");
 
   const addTask = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -14,27 +15,43 @@ export default function List() {
   };
 
   return (
-    <div className="list">
+    <div className="list w-64 mt-6 drop-shadow-lg">
       <div id="list-title" className="pb-4">
-        <h2 className=" text-secondary font-montserrat">My List</h2>
+        <input
+          className=" text-secondary font-montserrat list-input"
+          placeholder="List title"
+        />
       </div>
-      <form className="list-add-task" onSubmit={addTask}>
+      <ul>
+        {list.map((item, index) => (
+          <li
+            className="bg-bgSecondary py-4 px-5 mb-2 rounded-md drop-shadow-lg"
+            key={index}
+          >
+            <div className="flex flex-col">
+              <div className="bg-blue-500 py-2 w-max px-2 mb-2 text-xs drop-shadow-lg">
+                {Tags}
+              </div>
+              <p className="text-center mb-5">{item}</p>
+              <p className="text-center text-sm text-secondary uppercase">
+                In progress
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <form className="list-add-task mt-4" onSubmit={addTask}>
         <input
           type="text"
           placeholder="Add a task"
           value={task}
           onChange={(e) => setTask(e.target.value)}
-          className="list-input"
+          className="list-input text-primary"
         />
-        <button type="submit" className="btn-comp">
+        <button type="submit" className="drop-shadow-lg pl-6 btn-comp text-xs">
           Add Task
         </button>
       </form>
-      <ul>
-        {list.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
     </div>
   );
 }
--- BuildIT-Frontend/src/pages/project/Board.tsx
+++ BuildIT-Frontend/src/pages/project/Board.tsx
@@ -1,13 +1,12 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import List from "../../components/List";
 import "../../App.css";
 
 export default function Board() {
-  const { board_name } = useParams();
+  // const { board_name } = useParams();
 
   return (
     <div>
-      <h3 className="bg-bgSecondary relative">{board_name}</h3>
       <List />
     </div>
   );
