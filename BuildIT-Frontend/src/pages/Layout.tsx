7b099d80df7198c1af7f2ccfff3fa9e2441fb82c
Julien Charp <j.charpentier@outlook.fr>
2024-12-13 16:56:02 +0100

    Add comp List

--- BuildIT-Frontend/src/pages/Layout.tsx
+++ BuildIT-Frontend/src/pages/Layout.tsx
@@ -1,11 +1,10 @@
 import { Link, Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import { useState } from "react";
-import logo from '/buildit-logo.png';
+import logo from "/buildit-logo.png";
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
 
 export default function Layout() {
-
   const [isNavbarOpen, setIsNavbarOpen] = useState<boolean>(true);
 
   const toggleNavbar = () => {
@@ -22,20 +21,34 @@ export default function Layout() {
       >
         <Header isNavbarOpen={isNavbarOpen} toggleNavbar={toggleNavbar} />
       </div>
-      <div className={`${
+      <div
+        className={`${
           isNavbarOpen ? "w-10/12" : "w-full"
-        } p-16 transition-all duration-300 ease-in-out`} id="content">
+        } p-16 transition-all duration-300 ease-in-out overflow-x-auto`}
+        id="content"
+      >
         <Outlet />
       </div>
-      <div className={`w-full absolute transition-all duration-300 ease-in-out select-none`}>
-        <div className={`w-2/12 flex items-center justify-between p-4 transition-opacity duration-300 ease-in-out ${isNavbarOpen ? "opacity-0" : "opacity-100"}`}>
+      <div
+        className={`w-full absolute transition-all duration-300 ease-in-out select-none`}
+      >
+        <div
+          className={`w-2/12 flex items-center justify-between p-4 transition-opacity duration-300 ease-in-out ${
+            isNavbarOpen ? "opacity-0" : "opacity-100"
+          }`}
+        >
           <Link to="/" className="flex items-center">
             <img src={logo} className="w-8" alt="BuildIT logo" />
-            <h1 className="text-2xl text-primary ml-2 font-bold">BUILD<span className="text-secondary">IT</span></h1>
+            <h1 className="text-2xl text-primary ml-2 font-bold">
+              BUILD<span className="text-secondary">IT</span>
+            </h1>
           </Link>
-          <ChevronRightIcon onClick={toggleNavbar} className="w-4 h-4 cursor-pointer" />
+          <ChevronRightIcon
+            onClick={toggleNavbar}
+            className="w-4 h-4 cursor-pointer"
+          />
         </div>
       </div>
     </div>
-  )
+  );
 }
