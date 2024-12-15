6c7ce3e726cab6c6091553d53e03a08004ec3681
Theo <theo.richard8654@gmail.com>
2024-12-15 18:12:49 +0100

    Nettoyage

--- Backend/BuildIT_Backend/BuildIT_API/views.py
+++ Backend/BuildIT_Backend/BuildIT_API/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
 from .models.Users import Users
-from rest_framework.decorators import api_view
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -23,18 +22,3 @@ class RegisterView(generics.CreateAPIView):
         }
         headers = self.get_success_headers(serializer.data)
         return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
-
-@api_view(['POST'])
-def register(request):
-    if request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            refresh = RefreshToken.for_user(user)
-            response_data = {
-                'user': serializer.data,
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            }
-            return Response(response_data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
