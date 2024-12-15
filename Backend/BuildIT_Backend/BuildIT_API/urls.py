1e5a6856fe30e03e7ee4dc2acc24a7d9388c1c5c
Louis E <enjules31@gmail.com>
2024-12-15 22:11:09 +0100

    CRUD User OK

--- Backend/BuildIT_Backend/BuildIT_API/urls.py
+++ Backend/BuildIT_Backend/BuildIT_API/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from BuildIT_API.views.users_views import UserListCreateView, UserRetrieveUpdateDeleteView, UserLoginView, ProtectedView
+from BuildIT_API.views.users_views import  UserLoginView, ProtectedView, UserCreateView, UserDeleteView, UserRetrieveView, UserUpdateView
 
+userpatterns = [
+    path('register/', UserCreateView.as_view(), name='register'),               # Créer un utilisateur
+    path('get/user/<int:pk>/', UserRetrieveView.as_view(), name='user-detail'), # Rechercher un utilisateur par son id
+    path('update/', UserUpdateView.as_view(), name='user-update'),              # Modifier un utilisateur
+    path('delete/<int:pk>/', UserDeleteView.as_view(), name='user-delete'),     # Supprimer un utilisateur par son id
+    path('login/', UserLoginView.as_view(), name='user-login'),                 # Connexion d'un utilisateur
+]
 
 urlpatterns = [
-    path('users/', UserListCreateView.as_view(), name='user-list-create'),  # Lister/Créer des utilisateurs
-    path('users/<int:pk>/', UserRetrieveUpdateDeleteView.as_view(), name='user-detail'),  # CRUD pour un utilisateur
-    path('login/', UserLoginView.as_view(), name='user-login'),
-    path('protected/', ProtectedView.as_view(), name='protected-view'),
-]
+    path('protected/', ProtectedView.as_view(), name='protected-view')          # Route de test pour vérifier le fonctionnement des tokens
+] + userpatterns
