5281afa931c6019a8f4466cc199004682c4782c0
Theo <theo.richard8654@gmail.com>
2024-12-16 16:25:02 +0100

    Add CORS support and new API endpoints for thread management

--- Backend/BuildIT_Backend/BuildIT_API/urls.py
+++ Backend/BuildIT_Backend/BuildIT_API/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from BuildIT_API.views.users_views import  UserLoginView, ProtectedView, UserCreateView, UserDeleteView, UserRetrieveView, UserUpdateView
+from BuildIT_API.views.modale_ia_views import CreateThread, UpdateRunThread, GetAssistantResponse
 
 userpatterns = [
     path('register/', UserCreateView.as_view(), name='register'),               # Créer un utilisateur
@@ -7,6 +8,10 @@ userpatterns = [
     path('update/', UserUpdateView.as_view(), name='user-update'),              # Modifier un utilisateur
     path('delete/<int:pk>/', UserDeleteView.as_view(), name='user-delete'),     # Supprimer un utilisateur par son id
     path('login/', UserLoginView.as_view(), name='user-login'),                 # Connexion d'un utilisateur
+
+    path('create-thread', CreateThread.as_view(), name='create-thread'),        # Créer un thread
+    path('update-run-thread', UpdateRunThread.as_view(), name='update-run-thread'), # Mettre à jour un thread
+    path('get-assistant-response', GetAssistantResponse.as_view(), name='get-assistant-response'), # Récupérer la réponse de l'assistant
 ]
 
 urlpatterns = [
