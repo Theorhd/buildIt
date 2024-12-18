a21050d18930466c20d366cb6cf2820803f192c8
Louis E <enjules31@gmail.com>
2024-12-18 23:44:39 +0100

    Maj commentaires

--- Backend/BuildIT_Backend/BuildIT_API/urls.py
+++ Backend/BuildIT_Backend/BuildIT_API/urls.py
@@ -3,20 +3,20 @@ from BuildIT_API.views.users_views import  UserLoginView, ProtectedView, UserCre
 from BuildIT_API.views.projects_views import ProjectCreateView, ProjectRetriveView, ProjectDeleteView, ProjectUpdateView
 
 userpatterns = [
-    path('user/create/', UserCreateView.as_view(), name='register'),                # Créer un utilisateur
-    path('user/get/<int:pk>/', UserRetrieveView.as_view(), name='user-detail'),     # Rechercher un utilisateur par son id
-    path('user/update/', UserUpdateView.as_view(), name='user-update'),             # Modifier un utilisateur
-    path('user/delete/<int:pk>/', UserDeleteView.as_view(), name='user-delete'),    # Supprimer un utilisateur par son id
-    path('user/login/', UserLoginView.as_view(), name='user-login'),                # Connexion d'un utilisateur
+    path('user/create/', UserCreateView.as_view(), name='register'),                        # Créer un utilisateur
+    path('user/get/<int:pk>/', UserRetrieveView.as_view(), name='user-detail'),             # Rechercher un utilisateur par son id
+    path('user/update/', UserUpdateView.as_view(), name='user-update'),                     # Modifier un utilisateur
+    path('user/delete/<int:pk>/', UserDeleteView.as_view(), name='user-delete'),            # Supprimer un utilisateur par son id
+    path('user/login/', UserLoginView.as_view(), name='user-login'),                        # Connexion d'un utilisateur
 ]
 
 projectpatterns = [
-    path('project/create/', ProjectCreateView.as_view(), name='project-create'),    # Créer un projet
-    path('project/get/<int:pk>/', ProjectRetriveView.as_view(), name='project-detail'), # Rechercher un projet par son id
-    path('project/update/', ProjectUpdateView.as_view(), name='project-update'),     # Modifier un projet
-    path('project/delete/<int:pk>/', ProjectDeleteView.as_view(), name='project-delete'), # Supprimer un projet par son id
+    path('project/create/', ProjectCreateView.as_view(), name='project-create'),            # Créer un projet (accepte board, lists, items)
+    path('project/get/<int:pk>/', ProjectRetriveView.as_view(), name='project-detail'),     # Rechercher un projet par son id + token de ID créaeur
+    path('project/update/', ProjectUpdateView.as_view(), name='project-update'),            # Modifier un projet + token de ID créateur
+    path('project/delete/<int:pk>/', ProjectDeleteView.as_view(), name='project-delete'),   # Supprimer un projet par son id + token de ID créatieur
 ]
 
 urlpatterns = [
-    path('protected/', ProtectedView.as_view(), name='protected-view')              # Route de test pour vérifier le fonctionnement des tokens
+    path('protected/', ProtectedView.as_view(), name='protected-view')                      # Route de test pour vérifier le fonctionnement des tokens
 ] + userpatterns + projectpatterns
