ae21379d84ca57bd97bdb6bf1d3b9c667a958aab
Louis E <enjules31@gmail.com>
2024-12-19 14:52:39 +0100

    Fix route register -> create

--- BuildIT-Frontend/src/pages/Auth/Register.tsx
+++ BuildIT-Frontend/src/pages/Auth/Register.tsx
@@ -36,7 +36,7 @@ const Register: React.FC = () => {
         }
         const { confirmPassword, ...dataToSend } = formData;
         try {
-            const response = await axios.post(`${BackendUrl}api/user/register/`, dataToSend);
+            const response = await axios.post(`${BackendUrl}api/user/create/`, dataToSend);
             console.log(response.data);
 
             localStorage.setItem('access', response.data.tokens.access);
