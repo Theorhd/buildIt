e6d4fda06af3c27fa6435e098e2f19926e52fa87
maelbel <mael.belliard@laposte.net>
2025-01-13 11:59:31 +0100

    Fix infinity loop

--- BuildIT-Frontend/src/components/header/Navbar.tsx
+++ BuildIT-Frontend/src/components/header/Navbar.tsx
@@ -1,11 +1,38 @@
 import ProjectLink from "./ProjectLink";
 import {getProjectsFromToken} from "../../utils/api_router";
 import { ProjectInterface } from "../../utils/interfaces";
-
-const projects: ProjectInterface[] = await getProjectsFromToken();
-console.log(projects);
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+
+  const [projects, setProjects] = useState<ProjectInterface[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  // Utilisation de useEffect pour récupérer les projets
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const fetchedProjects = await getProjectsFromToken();
+        setProjects(fetchedProjects);
+      } catch (err) {
+        setError("Failed to load projects : " + err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProjects();
+  }, []); // Le tableau vide [] garantit que l'effet est exécuté une seule fois
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
     <nav>
         {projects && projects.length > 0 && projects.map((project) => (
